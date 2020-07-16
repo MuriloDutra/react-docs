6c29f6cdf510ac1bfabf063530107e6541a51770
Murilo <murilo.alves@watermelontecnologia.com>
2020-07-15 23:31:34 -0300

    New imports

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header'
+import React from 'react'
+import './App.css'
 import Clock from './components/Clock'
-import Reservation from './components/Reservation';
-import Calculator from './components/Calculator';
+import Title from './components/Title'
+import Glossario from './components/Accessiblity/Glossario'
+import Form from './components/Accessiblity/Form/Forms'
 
 
 function App() {
   return (
     <div className="App">
       <div className="body">
-        <Header title="Welcome to this page" />
+        <Title subtitle="How are you?">
+          <h1>Welcome to this page!</h1>
+        </Title>
         <Clock />
-        <Reservation />
-        <Calculator />
+        <Glossario />
+        <Form />
       </div>
     </div>
   );
 }
-//CHECKPOINT: https://pt-br.reactjs.org/docs/composition-vs-inheritance.html 
+//CHECKPOINT: https://pt-br.reactjs.org/docs/accessibility.html#mouse-and-pointer-events 
 export default App;
