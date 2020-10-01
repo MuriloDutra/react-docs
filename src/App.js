651f6fdb2b8d98b9661bd72a412928fd1ea8334b
Murilo <murilo.alves@watermelontecnologia.com>
2020-09-30 21:11:27 -0300

    Modules - profiler and react without ES6 - DONE

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { createRef } from 'react'
+import React, { createRef, Profiler } from 'react'
 import Clock from './components/Clock'
 import Title from './components/Title'
 import ErrorBoundary from './components/ErrorBoundaries/ErrorBoundary';
@@ -6,21 +6,38 @@ import FancyButton from './components/REFs/FancyButton';
 
 
 class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.onRanderCallback = this.onRanderCallback.bind(this)
+  }
+
+  onRanderCallback(id, phase, actualDuration, baseDuration, startTime, commitTime, interactions){
+    /*console.log('id: ', id)//id - o prop "id" da árvore Profiler que acabou de atualizar 
+    console.log('phase: ', phase)//phase - "mount" (se a árvore acabou de ser montada) ou "update" (se foi renderizada novamente)
+    console.log('actualDuration: ', actualDuration)//actualDuration - tempo gasto renderizando a atualização enviada
+    console.log('baseDuration: ', baseDuration)//baseDuration - tempo estimado para renderizar totalmente a subárvore sem memorização
+    console.log('startTime: ', startTime)//startTime - quando o React começou renderizar essa atualização
+    console.log('commitTime: ', commitTime)//commitTime - quando o React enviou essa atualização
+    console.log('interactions: ', interactions)//interactions - um Set de interações pertencentes â essa atualização*/
+  }
+
   render(){
     const buttonRef = createRef()
 
     return (
-      <ErrorBoundary>
-        <Title subtitle="How are you?">
-          <h1>Welcome to this page!</h1>
-        </Title>
-        <Clock />
-        <FancyButton ref={buttonRef} label="Click here!" />
-      </ErrorBoundary>
+      <Profiler id='mainProfiler' onRender={this.onRanderCallback}>
+        <ErrorBoundary>
+          <Title subtitle="How are you?">
+            <h1>Welcome to this page!</h1>
+          </Title>
+          <Clock />
+          <FancyButton ref={buttonRef} label="Click here!" />
+        </ErrorBoundary>
+      </Profiler>
     );
   }
 }
 
 
-//CHECKPOINT: https://pt-br.reactjs.org/docs/optimizing-performance.html
-export default App;
+//CHECKPOINT: https://pt-br.reactjs.org/docs/react-without-jsx.html
+export default App
