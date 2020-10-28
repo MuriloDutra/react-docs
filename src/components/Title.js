9222a3641d4d93fd1e7795130281fe984c3c1bac
Murilo <murilo.alves@watermelontecnologia.com>
2020-10-27 22:38:43 -0300

    TypeChecking module - DONE

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import PropTypes from 'prop-types';
+
 
 class Title extends React.Component{
 
@@ -11,5 +13,17 @@ class Title extends React.Component{
         )
     }
 }
+
+
+Title.defaultProps = {
+    subtitle: 'You have to provide "subtitle" property to <Title />'
+}
+
+
+Title.propTypes = {
+    subtitle: PropTypes.string,
+    children: PropTypes.element.isRequired  //Single child - This must be exactly one element or it will warn.
+}
+
 
 export default Title
